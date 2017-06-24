ee8b57efc0008b5874fc9287b952ec2d5ca06f78
yann <yann.boisselier@gmail.com>
2017-06-24 13:17:38 +0200

    deploy

--- sw.js
+++ sw.js
@@ -19,48 +19,44 @@ importScripts('workbox-sw.prod.v1.0.1.js');
  */
 const fileManifest = [
   {
-    "url": "/_nuxt/0.nuxt.bundle.5680b7a960506f8cd9c2.js",
-    "revision": "67481e8a6f5f9a7090df173bb1c4d1b5"
+    "url": "/_nuxt/0.nuxt.bundle.bd8f4217b134a1427460.js",
+    "revision": "d097c98563bed6d867023d14fe7fe669"
   },
   {
-    "url": "/_nuxt/1.nuxt.bundle.0a5685937702c370244f.js",
-    "revision": "98d92f38714caca43b1c1d8eefeb77ab"
+    "url": "/_nuxt/1.nuxt.bundle.cda29518b94fe697a17f.js",
+    "revision": "5c609ee9f650e6147f7636f7ef5960ac"
   },
   {
-    "url": "/_nuxt/2.nuxt.bundle.f0045773205d93c9fc39.js",
-    "revision": "bd73b31735448f0fab9d02d2edf0db35"
-  },
-  {
-    "url": "/_nuxt/3.nuxt.bundle.fdfbe50004513dffcf16.js",
-    "revision": "bddbffc38fc52f1b517b888d22d66a58"
+    "url": "/_nuxt/2.nuxt.bundle.e510cb7b12bda091aacd.js",
+    "revision": "d7ca90dbdc2c339db43ce0204d26cfcb"
   },
   {
     "url": "/_nuxt/client-manifest.json",
-    "revision": "9d436b3f524dc95b439c26d3e5a4d316"
+    "revision": "032307fb890dd76770cd2d310ab538f6"
   },
   {
     "url": "/_nuxt/index.html",
     "revision": "744fb1c184ce0e85af057191b683d6a2"
   },
   {
-    "url": "/_nuxt/manifest.df653018f3dd463ac46f.js",
-    "revision": "ece2a1434659e7163714fd89519c4e2c"
+    "url": "/_nuxt/manifest.97748513a51c972af34f.js",
+    "revision": "768960b755fba115f8306f6c236b6193"
   },
   {
     "url": "/_nuxt/manifest.ec0cab88.json",
     "revision": "8f9eb6d2f7446e0dc30edb3f792108aa"
   },
   {
-    "url": "/_nuxt/nuxt.bundle.d7d5a6ffcb12ac9a5c34.js",
-    "revision": "322d69a4c7e6608c6d9b848d58f5ae8b"
+    "url": "/_nuxt/nuxt.bundle.30d36fed0dd2823cc347.js",
+    "revision": "172586d41caf982e06b02ef21646040f"
   },
   {
     "url": "/_nuxt/server-bundle.json",
-    "revision": "e34738b59d542a93b4b616398ed032e0"
+    "revision": "bd1fc05f493b86f8cf838d4a97c6140a"
   },
   {
-    "url": "/_nuxt/vendor.bundle.ad611c574876b601bef0.js",
-    "revision": "3c533dbb3977b40d6d015ecadf937367"
+    "url": "/_nuxt/vendor.bundle.6e11a1538b3dc7aed1d4.js",
+    "revision": "208e7b96b9ad18ddb08909b4e5852167"
   }
 ];
 
